6d5eb1ce4b838af3d8aec1129dc87e80b0f2292b
anathayna <anathaynafranca@gmail.com>
2020-04-13 13:09:47 -0300

    adding func2

---
+++ iOS-Bootcamp-Function-2.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+func calculator() {
+  let a = Int(readLine()!)! //First input
+  let b = Int(readLine()!)! //Second input
+  
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: a, n2: b)
+  
+}
+
+func add(n1: Int, n2: Int) {
+  print(n1 + n2)
+}
+
+func subtract(n1: Int, n2: Int) {
+  print(n1 - n2)
+}
+
+func multiply(n1: Int, n2: Int) {
+  print(n1 * n2)
+}
+
+func divide(n1: Int, n2: Int) {
+  let decimalN1 = Double(n1)
+  let decimalN2 = Double(n2)
+  print(decimalN1 / decimalN2)
+}
+
+calculator()
