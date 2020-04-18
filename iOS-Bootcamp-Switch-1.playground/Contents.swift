793aea055248f5478c7822314ad415f04fbe7830
anathayna <anathaynafranca@gmail.com>
2020-04-18 20:40:45 -0300

    adding switch case challenge

---
+++ iOS-Bootcamp-Switch-1.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func loveCalculator() {
+    let loveScore = Int.random(in: 0...100)
+    
+    switch loveScore {
+    case 81...100:
+        print("You love each other like Kanye West loves Kanye")
+    case 41..<81:
+        print("You go together like Coke and Mentos")
+    case ...40:
+        print("You'll be forever alone, Banana")
+    default:
+        print("Error")
+    }
+    
+}
+
+loveCalculator()
