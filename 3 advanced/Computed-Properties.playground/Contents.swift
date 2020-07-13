5c64ff3106fef094afc298c8fdf8f41f89011023
anathayna <anathaynafranca@gmail.com>
2020-07-13 12:53:35 -0300

    adding computed properties

--- 3 advanced/Computed-Properties.playground/Contents.swift
+++ 3 advanced/Computed-Properties.playground/Contents.swift
@@ -1,12 +1,23 @@
 import Foundation
 
 let pizzaInInches: Int = 12
+var numberOfPeople: Int = 6
+let slicesPerPerson: Int = 3
 
 var numberOfSlices: Int {
     get { return pizzaInInches - 4 }
-    set { print("numberOfSlices now has a new value which is \(newValue)") }
 }
 
-numberOfSlices = 10
+var numberOfPizza: Int {
+    get {
+        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        return numberOfSlices / numberOfPeopleFedPerPizza
+    }
+    set {
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
+    }
+}
 
-print(numberOfSlices)
+numberOfPizza = 4
+print(numberOfPeople)
