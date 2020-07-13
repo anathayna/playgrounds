4cb05e2e5841aba4b91a1c32319a06b4a7070116
anathayna <anathaynafranca@gmail.com>
2020-07-13 12:42:24 -0300

    adding setter

--- 3 advanced/Computed-Properties.playground/Contents.swift
+++ 3 advanced/Computed-Properties.playground/Contents.swift
@@ -1,7 +1,12 @@
 import Foundation
 
 let pizzaInInches: Int = 12
+
 var numberOfSlices: Int {
     get { return pizzaInInches - 4 }
+    set { print("numberOfSlices now has a new value which is \(newValue)") }
 }
+
+numberOfSlices = 10
+
 print(numberOfSlices)
