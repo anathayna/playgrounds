33a4a9426f139854a2c2d61b7aa2d255834c3556
ana thayna <ana.thayna@enjoei.com.br>
2022-04-07 12:46:12 -0300

    :computer: development(beginner): finish simple values examples

--- 1 beginner/Simple-Values.playground/Contents.swift
+++ 1 beginner/Simple-Values.playground/Contents.swift
@@ -30,3 +30,30 @@ let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 let name = "Anne"
 let calculation = "calculation result: \(9.9/3.3)"
 let greeting = "Hello! I'm \(name)"
+
+// strings with multiple lines
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+// arrays and dictionaries
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupations["Jayne"] = "Public Relations"
+
+shoppingList.append("blue paint")
+print(shoppingList)
+
+let emptyArray: [String] = []
+let emptyDictionary: [String: Float] = [:]
+
+shoppingList = []
+occupations = [:]
