7ed49c146791ed6ebcfc7f61cf0bf9f2aeb57a82
ana thayna <ana.thayna@enjoei.com.br>
2022-04-27 12:15:36 -0300

    :books: study(swift): adding tuple examples

--- 2 intermediate/Functions-Closures.playground/Contents.swift
+++ 2 intermediate/Functions-Closures.playground/Contents.swift
@@ -14,3 +14,30 @@ func greet3(_ person: String, on day: String) -> String {
     return "Hello \(person), today is \(day)."
 }
 greet3("John", on: "Wednesday")
+
+// tuple
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
+print(statistics.max)
+print(statistics.1)
+print(statistics.min)
+print(statistics.0)
