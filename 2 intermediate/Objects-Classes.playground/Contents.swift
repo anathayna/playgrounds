4ffe98188aa71af6e48e897f853653a1e74e27e4
ana thayna <ana.thayna@enjoei.com.br>
2022-05-03 12:53:19 -0300

    :books: study(swift): starting objects and classes examples

---
+++ 2 intermediate/Objects-Classes.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class Shape {
+    var numberOfSides1 = 0
+    let numberOfSides2 = 0
+    
+    func simpleDescription1() -> String {
+        return "A shape with \(numberOfSides1) sides."
+    }
+    
+    func simpleDescription2() -> String {
+        return "A shape with \(numberOfSides2) sides."
+    }
+}
