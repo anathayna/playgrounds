d97981b2bbb860d1509a157670ecc3c1e9634d19
ana thayna <ana.thayna@enjoei.com.br>
2022-05-09 14:39:38 -0300

    :books: study(swift): adding override example

--- 2 intermediate/Objects-Classes.playground/Contents.swift
+++ 2 intermediate/Objects-Classes.playground/Contents.swift
@@ -37,3 +37,50 @@ class NamedShape {
 var namedShape = NamedShape(name: "etc")
 namedShape.numberOfSides = 7
 var shapeDescription2 = namedShape.simpleDescription()
+
+// override
+
+class Square: NamedShape {
+    var sideLength: Double
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+class Circle: NamedShape {
+    var radius: Double
+    var π: Double = 3.1416
+
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+        numberOfSides = 4
+    }
+
+    func area() -> Double {
+        return π * (radius * radius) // math.pi
+    }
+
+    override func simpleDescription() -> String {
+        return "A circle with a radius of \(radius)."
+    }
+}
+
+let circle = Circle(radius: 5.2, name: "my test circle")
+circle.area()
+circle.simpleDescription()
