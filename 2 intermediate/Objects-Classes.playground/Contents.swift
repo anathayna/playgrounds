5a61e2f12b35eb39406eafa45c7cd80deaac6abe
ana thayna <ana.thayna@enjoei.com.br>
2022-07-21 12:32:28 -0300

    :books: study(swift): adding get/set example

--- 2 intermediate/Objects-Classes.playground/Contents.swift
+++ 2 intermediate/Objects-Classes.playground/Contents.swift
@@ -84,3 +84,43 @@ class Circle: NamedShape {
 let circle = Circle(radius: 5.2, name: "my test circle")
 circle.area()
 circle.simpleDescription()
+
+// getter and setter
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0 // implicit name newValue
+        }
+    }
+    
+    /*
+     var perimeter: Double {
+         get {
+             return 3.0 * sideLength
+         }
+         set(new) { // explicit name
+             sideLength = new / 3.0
+         }
+     }
+     */
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
