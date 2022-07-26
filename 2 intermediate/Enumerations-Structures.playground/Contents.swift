5fef75e35be2f0772b43307aba4d2795a418d82c
ana thayna <ana.thayna@enjoei.com.br>
2022-07-26 13:04:34 -0300

    :books: study(swift): adding enumerations example

---
+++ 2 intermediate/Enumerations-Structures.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+let jack = Rank.jack
+let jackRawValue = jack.rawValue
+
+func comparesRank(a: Rank, b: Rank) -> Bool {
+    return a.rawValue > b.rawValue
+}
+
+comparesRank(a: jack, b: ace)
+
+// rawValue
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+}
