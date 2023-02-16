fbe7a5ff5a12426f6f3273ba35f072f0784e54c7
ana thayna <ana.thayna@enjoei.com.br>
2023-02-16 16:26:18 -0300

    :open_book: study(swift): adding interger bounds example

--- 1 beginner/Simple-Values.playground/Contents.swift
+++ 1 beginner/Simple-Values.playground/Contents.swift
@@ -57,3 +57,9 @@ let emptyDictionary: [String: Float] = [:]
 
 shoppingList = []
 occupations = [:]
+
+// integer bounds
+
+let minValue = UInt8.min  // minValue is equal to 0, and is of type UInt8
+let maxValue = UInt8.max  // maxValue is equal to 255, and is of type UInt8
+
