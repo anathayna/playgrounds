f930b62986e68b0bfa91629c84ae87a056cc9f3a
ana thayna <ana.thayna@enjoei.com.br>
2023-03-09 18:23:55 -0300

    :open_book: study(swift): adding more tuple examples

--- 3 advanced/Tuples.playground/Contents.swift
+++ 3 advanced/Tuples.playground/Contents.swift
@@ -1,11 +1,28 @@
 import Foundation
 
-let touple1 = ("Ana", 24)
-let touple2 = (name: "Ana", age: 24)
-let touple3: (name: String, age: Int)
+let tuple1 = ("Ana", 24)
+let tuple2 = (name: "Ana", age: 24)
+let tuple3: (name: String, age: Int)
 
-touple3 = (name: "Ana", age: 24)
+tuple3 = (name: "Ana", age: 24)
 
-print(touple1.1)
-print(touple2.name)
-print("\(touple3.name) is \(touple3.age) years old")
+print(tuple1.1)
+print(tuple2.name)
+print("\(tuple3.name) is \(tuple3.age) years old")
+
+let http404Error = (404, "Not Found") // tuple of type (Int, String)
+
+let (statusCode, statusMessage) = http404Error // decompose a tuple’s contents
+print("The status code is \(statusCode)")
+print("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error // ignore parts of the tuple
+print("The status code is \(justTheStatusCode)")
+
+print("The status code is \(http404Error.0)") // access the individual element values in a tuple using index numbers
+print("The status message is \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
