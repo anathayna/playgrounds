7a3c86ee53813e2e8571eece884879325f4576c3
ana thayna <ana.thayna@enjoei.com.br>
2023-03-16 15:53:39 -0300

    :open_book: study(swift): correcting optional examples and adding some more

--- 2 intermediate/Optional.playground/Contents.swift
+++ 2 intermediate/Optional.playground/Contents.swift
@@ -1,29 +1,33 @@
-import UIKit
+import Foundation
 
-var studentsAndScores = ["Amy": Int(readLine()!)!, "James": Int(readLine()!)!, "Helen": Int(readLine()!)!]
+var studentsAndScores = ["Amy": Int(readLine() ?? ""),
+                         "James": Int(readLine() ?? ""),
+                         "Helen": Int(readLine() ?? "")]
 
-func highestScore(scores: [String: Int]) {
-
-  let a = studentsAndScores["Amy"]!
-  let b = studentsAndScores["James"]!
-  let c = studentsAndScores["Helen"]!
-  
-  var temp = 0
+func highestScore(scores: [String: Int?]) {
     
-  if a > temp {
-    temp = a
-  }
+    guard let a = studentsAndScores["Amy"] else { return }
+    guard let b = studentsAndScores["James"] else { return }
+    guard let c = studentsAndScores["Helen"] else { return }
     
-  if b > temp {
-    temp = b
-  }
-  
-  if c > temp {
-    temp = c
-  }
-  
-  print(temp)
-   
+    var temp = 0
+    
+    if let amy = a, amy > temp { temp = amy }
+    if let james = b, james > temp { temp = james }
+    if let helen = c, helen > temp { temp = helen }
+    
+    print(temp)
+    
 }
 
 highestScore(scores: studentsAndScores)
+
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+// nil
+
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var surveyAnswer: String? // is automatically set to nil
